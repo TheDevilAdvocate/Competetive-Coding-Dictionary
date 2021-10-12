6e75349da2ac45954c593ed9cd876f0804f05c4f
Akshay Pratap Singh <86912953+TheDevilAdvocate@users.noreply.github.com>
2021-10-12 17:16:35 +0530

    Create FirstOccourenceBinarySearch.cpp

---
+++ FirstOccourenceBinarySearch.cpp
@@ -0,0 +1,26 @@
+//Array must be sorted
+ll FirstOccourence(ll *a, ll size, ll key)
+{
+    ll sp = 0;
+    ll ep = size - 1;
+    ll ans = -1;
+    while (sp <= ep)
+    {
+        //**IMPORTANT**
+        ll mid=(sp+ep)/2; 
+        if (a[mid] == key)
+        {
+            ans = mid;
+            ep = mid - 1;
+        }
+        else if (a[mid] > key)
+        {
+            ep = mid - 1;
+        }
+        else if (a[mid] < key)
+        {
+            sp = mid + 1;
+        }
+    }
+    return ans;
+}

10942055830982f8a688ff74a0172c5db567b443
Akshay Pratap Singh <86912953+akshayiitism@users.noreply.github.com>
2021-11-29 21:30:19 +0530

    Update CP_Template.cpp

--- CP_Template.cpp
+++ CP_Template.cpp
@@ -1,24 +1,41 @@
 #include <bits/stdc++.h>
 #define ll long long int
 #define ull unsigned long long int
-#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define fastread() (ios_base::sync_with_stdio(false), cin.tie(NULL));
 #define pb push_back
 #define pf push_front
 #define eb emplace_back
-#define loop(i,n) for(ll i=0;i<(n);i++)
-#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
-#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define loop(i, n) for (ll i = 0; i < (n); i++)
+#define FOR(i, a, b) for (ll i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (ll i = (a); i >= (b); i--)
 #define EPS 1e-9
 #define PI 3.1415926535897932384626433832795
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 using namespace std;
+/******READER******/
+template <typename... T>
+void read(T &...args) {
+((cin >> args), ...);
+}
+/******DEBUGGER******/
+#define deb(...) logger(#__VA_ARGS__, __VA_ARGS__)
+template <typename... Args>
+void logger(string vars, Args &&...values)
+{
+    cout << vars << " = ";
+    string delim = "";
+    (..., (cout << delim << values, delim = ", "));
+}
+/*--------------------------------------------------*/
 int main()
 {
-fastread();
-int t;
-cin>>t;
-while(t--){
-
-
-}
-return 0;
+    fastread();
+    ll t;
+    cin>>t;
+    while (t--)
+    {
+        
+    }
+    return 0;
 }
